9d99dad1aa93bbd6a23f11c476908ad6fe321a4e
miltonszistea <milton.szpryngier@istea.com.ar>
2023-07-07 19:24:22 -0300

    Added Data Annotations

--- FinalHP/Models/Community.cs
+++ FinalHP/Models/Community.cs
@@ -1,13 +1,21 @@
-
+using System.ComponentModel.DataAnnotations;
 using FinalHP.Utils;
 namespace FinalHP;
 
 public class Community 
 {
     public int Id { get; set; }
+    [Required]
+    [Display(Name = "Nombre de la Comunidad")]
     public string Name { get; set; }
+    [Required]
+    [Display(Name = "País")]
     public string Country { get; set; }
+    [Required]
+    [Display(Name = "Tipo de Comunidad")]
     public CommunityType CommunityType { get; set; }
+    [Required]
+    [Display(Name = "Dirección")]
     public string Address { get; set; }
 
     public virtual List<Product> Products { get; set; } = new List<Product>();
--- FinalHP/Models/Product.cs
+++ FinalHP/Models/Product.cs
@@ -1,14 +1,24 @@
-
+using System.ComponentModel.DataAnnotations;
 using FinalHP.Utils;
 namespace FinalHP;
 
 public class Product 
 {
     public int Id { get; set; }
+    [Required]
+    [Display(Name = "Nombre")]
     public string Name { get; set; }
+    [Required]
+    [Display(Name = "Precio")]
     public decimal Price { get; set; }
+    [Required]
+    [Display(Name = "Tipo de producto")]
     public ProductType Type { get; set; }
+    [Required]
+    [Display(Name = "Disponible para envío")]
     public bool shippingAvailable { get; set; } = false;
+    [Required]
+    [Display(Name = "Cantidad vendida")]
     public int QuantitySold {get; set;}
     public virtual List<Community> Communities { get; set; }
 }

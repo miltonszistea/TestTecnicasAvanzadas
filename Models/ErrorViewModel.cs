14ffce657d65f16417607861e607a90873553573
miltonszistea <milton.szpryngier@istea.com.ar>
2023-07-09 15:32:32 -0300

    ChangedStructure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinalHerramientas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
